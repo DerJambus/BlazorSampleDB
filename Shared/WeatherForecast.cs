d228e650ef7783c8ac9e822667d7412922e83244
DerJambus <mex128@outlook.com>
2021-09-30 12:40:39 +0200

    Startpoint for the project

--- Shared/WeatherForecast.cs
+++ Shared/WeatherForecast.cs
@@ -6,6 +6,7 @@ namespace BlazorSampleDB.Shared
 {
     public class WeatherForecast
     {
+        public int Id { get; set; }
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
